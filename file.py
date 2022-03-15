1a6f6ca3f1d9133b9467a7dd170e9f58a2f69043
todianye <toporkov.daniil528@mail.ru>
2022-03-15 18:18:48 +0300

    Create file.py
    file study

---
+++ file.py
@@ -0,0 +1,5 @@
+x='10'
+y='cold'
+print ('Wheather: temperature +{} and it\'s {}'.format(x,y))
+print ('Wheather: temperature +{1} and it\'s {0}'.format(x,y))
+print (f'Wheather: temperature +{x} and it\'s {y}')
